1663abe7cbcdbfacfa603cc48ea43b8958ce22a8
Jay Potter <j@ypotter.ca>
2024-03-23 15:31:01 -0400

    Update ContainerAwareInterface.php

--- src/Potter/Container/Aware/ContainerAwareInterface.php
+++ src/Potter/Container/Aware/ContainerAwareInterface.php
@@ -1,16 +1,12 @@
 <?php
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPInterface.php to edit this template
- */
+declare(strict_types=1);
 
 namespace Potter\Container\Aware;
 
-/**
- *
- * @author jay
- */
-interface ContainerAwareInterface {
-    //put your code here
+use \Potter\Aware\AwareInterface;
+
+interface ContainerAwareInterface extends AwareInterface
+{
+    public function hasContainer(): bool;
 }
