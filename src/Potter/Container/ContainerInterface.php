86d5adee3cb489385dddd8d0f56d04fc8fb59c4b
Jay Potter <j@ypotter.ca>
2025-07-01 19:07:25 -0400

    Create ContainerInterface.php

---
+++ src/Potter/Container/ContainerInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Potter\Container;
+
+interface ContainerInterface
+    extends \Illuminate\Contracts\Container\Container,
+            \Psr\Container\ContainerInterface
+{ }
